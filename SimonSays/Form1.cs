b843facfbf0acdd2faea76e6298edc9ee0a0a046
Aubrey Carter <aubrcart224@ed.amdsb.ca>
2022-02-22 10:01:44 -0500

    day 1. save

--- SimonSays/Form1.cs
+++ SimonSays/Form1.cs
@@ -15,15 +15,64 @@ namespace SimonSays
     public partial class Form1 : Form
     {
         //TODO: create a List to store the pattern. Must be accessable on other screens
+        public static List<int> pattern = new List<int>();
+
 
         public Form1()
         {
             InitializeComponent();
+            ChangeScreen(this, new MenuScreen());
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //TODO: Launch MenuScreen
+
+
         }
-    }
+
+        public static void ChangeScreen(object sender, UserControl next)
+        {
+
+            {
+
+                Form f; // will either be the sender or parent of sender 
+
+
+
+                if (sender is Form)
+
+                {
+
+                    f = (Form)sender;                          //f is sender 
+
+                }
+
+                else
+
+                {
+
+                    UserControl current = (UserControl)sender;  //create UserControl from sender 
+
+                    f = current.FindForm();                     //find Form UserControl is on 
+
+                    f.Controls.Remove(current);                 //remove current UserControl 
+
+                }
+
+
+
+                // add the new UserControl to the middle of the screen and focus on it 
+
+                next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+
+                    (f.ClientSize.Height - next.Height) / 2);
+
+                f.Controls.Add(next);
+
+                next.Focus();
+
+            }
+        }
+    } 
 }
--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -16,6 +16,12 @@ namespace SimonSays
     {
         //TODO: create guess variable to track what part of the pattern the user is at
 
+        //randoom number gen 
+        public static Random randNum = new Random();
+        int number = randNum.Next(0, 5);
+        //int number = 0;
+        
+
 
         public GameScreen()
         {
@@ -25,15 +31,44 @@ namespace SimonSays
         private void GameScreen_Load(object sender, EventArgs e)
         {
             //TODO: clear the pattern list from form1, refresh, pause for a bit, and run ComputerTurn()
+            Form1.pattern.Clear();
+            Thread.Sleep(1000);
         }
 
         private void ComputerTurn()
         {
             //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list
+            Form1.pattern.Add(number);
 
             //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            for (int i = 0; i < number; i++)
+            {
+                if(number == 0)
+                {
+                    greenButton.BackColor= Color.White;
+                    
+                }
+                else if (number == 1)
+                {
+                    blueButton.BackColor= Color.White;
+                    
+                }
+                else if (number == 2)
+                {
+                    redButton.BackColor= Color.White;
+                   
+                }
+                else if (number ==3)
+                {
+                    yellowButton.BackColor= Color.White;
+                    
+                }
+                Refresh();
+
+            }
 
             //TODO: get guess index value back to 0
+            number = 0;
         }
 
         public void GameOver()
@@ -41,6 +76,7 @@ namespace SimonSays
             //TODO: Play a game over sound
 
             //TODO: close this screen and open the GameOverScreen
+            Form1.ChangeScreen(this, new GameOverScreen());
 
         }
 
@@ -54,6 +90,9 @@ namespace SimonSays
                 // check to see if we are at the end of the pattern. If so:
                     // call ComputerTurn() method
                 // else call GameOver method
+
+
+
         }
     }
 }
